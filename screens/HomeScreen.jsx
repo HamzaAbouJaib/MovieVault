e011bcb7ce3b1b854eaba90953f20ba2a8fe6fdb
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-09 16:38:00 -0400

    Create home screen top navigation

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
+import {
+  Bars3BottomLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { primaryStyles } from "../themes/primary";
 
 export default HomeScreen = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-zinc-950 pt-6">
+      <SafeAreaView className="flex-row justify-between items-center mx-5">
+        <Bars3BottomLeftIcon color={"white"} size={30} />
+        <Text className="text-3xl font-semibold" style={primaryStyles.text}>
+          Streamify
+        </Text>
+        <MagnifyingGlassIcon color={"white"} size={25} strokeWidth={1.7} />
+      </SafeAreaView>
     </View>
   );
 };

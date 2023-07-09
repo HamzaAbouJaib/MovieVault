d3c5f9ca81371a7bed9efb5193eb7e3bacf863ac
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-09 15:02:51 -0400

    Create primary theme

---
+++ themes/primary.js
@@ -0,0 +1,9 @@
+export const primaryTheme = {
+  text: "#6be48b",
+  background: "#6be48b",
+};
+
+export const primaryStyles = {
+  text: { color: primaryTheme.text },
+  background: { backgorundColor: primaryTheme.background },
+};

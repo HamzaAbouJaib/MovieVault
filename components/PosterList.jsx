9a07308b2fa36eb3c5817536af53929368e3c3be
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-10 14:33:04 -0400

    Display items in a scrollable list

--- components/PosterList.jsx
+++ components/PosterList.jsx
@@ -1,9 +1,37 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image, Dimensions } from "react-native";
+import { image185 } from "../api/movies";
 
-export default PosterList = ({ data }) => {
+const { width, height } = Dimensions.get("window");
+
+export default PosterList = ({ title, data }) => {
   return (
-    <View>
-      <Text>PosterList</Text>
+    <View className="ml-5">
+      <Text className="text-white font-semibold text-lg">{title}</Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingTop: 12 }}
+      >
+        {data.map((item, index) => (
+          <View className="space-y-1 mr-4" key={index}>
+            <Image
+              source={{
+                uri: image185(item.poster_path) || fallbackMoviePoster,
+              }}
+              style={{
+                width: width * 0.3,
+                height: height * 0.2,
+                borderRadius: 8,
+              }}
+            />
+            <Text className="text-neutral-300 ml-1">
+              {item.title.length > 15
+                ? item.title.slice(0, 15) + "..."
+                : item.title}
+            </Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };

00e561237921a61644c77456b45bcd45e5e31482
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-11 15:31:51 -0400

    Create api for fetching tv series information

---
+++ api/tv.js
@@ -0,0 +1,91 @@
+import axios from "axios";
+import ApiKey from "../apiKey";
+
+const baseEndpoint = "https://api.themoviedb.org/3";
+const trendingTVsEndpoint = `${baseEndpoint}/trending/tv/day?api_key=${ApiKey}`;
+const topRatedTVsEndpoint = `${baseEndpoint}/tv/top_rated?api_key=${ApiKey}`;
+const airingTodayTVsEndpoint = `${baseEndpoint}/tv/airing_today?api_key=${ApiKey}`;
+
+const tvDetailsEndpoint = (id) => `${baseEndpoint}/tv/${id}?api_key=${ApiKey}`;
+const tvCreditsEndpoint = (id) =>
+  `${baseEndpoint}/tv/${id}/credits?api_key=${ApiKey}`;
+const similarTVsEndpoint = (id) =>
+  `${baseEndpoint}/tv/${id}/similar?api_key=${ApiKey}`;
+const personDetailsEndpoint = (id) =>
+  `${baseEndpoint}/person/${id}?api_key=${ApiKey}`;
+const personTVsEndpoint = (id) =>
+  `${baseEndpoint}/person/${id}/tv_credits?api_key=${ApiKey}`;
+const searchTVsEndpoint = (query) =>
+  `${baseEndpoint}/search/tv?query=${query}&include_adult=true&language=en-US&page=1&api_key=${ApiKey}`;
+const tvBannerImagesEndpoint = (id) =>
+  `${baseEndpoint}/tv/${id}/images'?api_key=${ApiKey}`;
+
+export const image500 = (path) =>
+  path ? `https://image.tmdb.org/t/p/w500${path}` : null;
+
+export const image342 = (path) =>
+  path ? `https://image.tmdb.org/t/p/w342${path}` : null;
+
+export const image185 = (path) =>
+  path ? `https://image.tmdb.org/t/p/w185${path}` : null;
+
+export const fallbackMoviePoster =
+  "https://img.myloview.com/stickers/white-laptop-screen-with-hd-video-technology-icon-isolated-on-grey-background-abstract-circle-random-dots-vector-illustration-400-176057922.jpg";
+export const fallbackPersonImage =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmUiF-YGjavA63_Au8jQj7zxnFxS_Ay9xc6pxleMqCxH92SzeNSjBTwZ0l61E4B3KTS7o&usqp=CAU";
+
+async function apiCall(endpoint, params) {
+  const options = {
+    method: "GET",
+    url: endpoint,
+    params: params ? params : {},
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return {};
+  }
+}
+
+export function fetchTrendingTVs() {
+  return apiCall(trendingTVsEndpoint);
+}
+
+export function fetchTopRatedTVs() {
+  return apiCall(topRatedTVsEndpoint);
+}
+
+export function fetchAiringTodayTVs() {
+  return apiCall(airingTodayTVsEndpoint);
+}
+
+export function fetchMovieDetails(id) {
+  return apiCall(tvDetailsEndpoint(id));
+}
+
+export function fetchMovieCredits(id) {
+  return apiCall(tvCreditsEndpoint(id));
+}
+
+export function fetchSimilarTVs(id) {
+  return apiCall(similarTVsEndpoint(id));
+}
+
+export function fetchPersonDetails(id) {
+  return apiCall(personDetailsEndpoint(id));
+}
+
+export function fetchPersonTVs(id) {
+  return apiCall(personTVsEndpoint(id));
+}
+
+export function fetchSearchedTVs(query) {
+  return apiCall(searchTVsEndpoint(query));
+}
+
+export function fetchMovieBannerImages(id) {
+  return apiCall(tvBannerImagesEndpoint(id));
+}

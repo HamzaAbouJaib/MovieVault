95d5f066b6616b9be6837600cfab071586b27a29
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-11 21:51:09 -0400

    Navigate to movie screen on card click

--- components/TrendingMoviesList.jsx
+++ components/TrendingMoviesList.jsx
@@ -8,14 +8,16 @@ import {
 } from "react-native";
 import Carousel, { Pagination } from "react-native-snap-carousel";
 import { LinearGradient } from "expo-linear-gradient";
-import { PlayIcon } from "react-native-heroicons/outline";
 import { primaryStyles } from "../themes/primary";
 import { image500 } from "../api/movies";
+import { useNavigation } from "@react-navigation/native";
 
 const { width, height } = Dimensions.get("window");
 
 export default TrendingMoviesList = ({ movies }) => {
   const [activeDotIndex, setActiveDotIndex] = useState(0);
+
+  const navigation = useNavigation();
 
   return (
     <View>
@@ -26,46 +28,50 @@ export default TrendingMoviesList = ({ movies }) => {
         data={movies}
         renderItem={({ item }) => {
           return (
-            <View style={{ height: height * 0.3 }}>
-              <Image
-                source={{
-                  uri: image500(item.backdrop_path),
-                }}
-                style={{ width: width * 0.95, height: height * 0.3 }}
-                resizeMode="cover"
-                className="overflow-hidden"
-              />
-              <LinearGradient
-                // Button Linear Gradient
-                colors={[
-                  "transparent",
-                  "rgba(24,24,27,0.7)",
-                  "rgba(24,24,27,0.9)",
-                  "rgba(24,24,27,1)",
-                ]}
-                style={{ width, height: height * 0.3 }}
-                start={{ x: 0.5, y: 0 }}
-                end={{ x: 0.5, y: 1 }}
-                className="absolute"
-              />
-              <View className="absolute bottom-0 mb-4 ml-5 z-20">
-                <View>
-                  <Text
-                    className="text-white text-lg font-semibold leading-5"
-                    style={{ width: width * 0.8 }}
-                  >
-                    {item.title}
-                  </Text>
-                  <Text
-                    className="text-neutral-500 font-semibold"
-                    style={{ fontSize: 12.5 }}
-                  >
-                    {item.release_date?.split("-")[0]} • {item.genres[0].name} •{" "}
-                    {item.runtime} min
-                  </Text>
+            <TouchableWithoutFeedback
+              onPress={() => navigation.navigate("MovieDetails", item)}
+            >
+              <View style={{ height: height * 0.3 }}>
+                <Image
+                  source={{
+                    uri: image500(item.backdrop_path),
+                  }}
+                  style={{ width: width * 0.95, height: height * 0.3 }}
+                  resizeMode="cover"
+                  className="overflow-hidden"
+                />
+                <LinearGradient
+                  // Button Linear Gradient
+                  colors={[
+                    "transparent",
+                    "rgba(24,24,27,0.7)",
+                    "rgba(24,24,27,0.9)",
+                    "rgba(24,24,27,1)",
+                  ]}
+                  style={{ width, height: height * 0.3 }}
+                  start={{ x: 0.5, y: 0 }}
+                  end={{ x: 0.5, y: 1 }}
+                  className="absolute"
+                />
+                <View className="absolute bottom-0 mb-4 ml-5 z-20">
+                  <View>
+                    <Text
+                      className="text-white text-lg font-semibold leading-5"
+                      style={{ width: width * 0.8 }}
+                    >
+                      {item.title}
+                    </Text>
+                    <Text
+                      className="text-neutral-500 font-semibold"
+                      style={{ fontSize: 12.5 }}
+                    >
+                      {item.release_date?.split("-")[0]} • {item.genres[0].name}{" "}
+                      • {item.runtime} min
+                    </Text>
+                  </View>
                 </View>
               </View>
-            </View>
+            </TouchableWithoutFeedback>
           );
         }}
         firstItem={0}

c7349fd35fea4acc05ff93260de19f18610c8b18
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-11 22:02:28 -0400

    Create top navigation and favourite icon

--- screens/MovieScreen.jsx
+++ screens/MovieScreen.jsx
@@ -1,9 +1,21 @@
-import { View, Text } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { View, Text, SafeAreaView } from "react-native";
+import { ArrowLeftIcon, HeartIcon } from "react-native-heroicons/outline";
 
 export default MovieScreen = () => {
+  const { params: item } = useRoute();
+
   return (
-    <View>
-      <Text>MovieScreen</Text>
+    <View className="flex-1 bg-zinc-950 pt-12">
+      <SafeAreaView className="flex-row justify-between items-center mx-5 mb-3">
+        <View className="bg-neutral-600 rounded-full p-2">
+          <ArrowLeftIcon color={"white"} size={25} strokeWidth={1.7} />
+        </View>
+        <View className="bg-neutral-600 rounded-full p-2">
+          <HeartIcon color={"white"} size={25} strokeWidth={1.7} />
+        </View>
+      </SafeAreaView>
+      <Text>{item.title}</Text>
     </View>
   );
 };

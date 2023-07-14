d7b9a4528b74085f2dc5c2366a240c9b97abe718
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-14 18:58:49 -0400

    Create text input component

--- screens/SearchScreen.jsx
+++ screens/SearchScreen.jsx
@@ -1,9 +1,33 @@
-import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { ArrowLeftIcon } from "react-native-heroicons/outline";
 
 export default SearchScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    <ScrollView className="flex-1 bg-zinc-950">
+      <SafeAreaView className="flex-row justify-between items-center mx-5 mb-3 z-20 mt-10">
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <View className="bg-neutral-800 rounded-full p-2">
+            <ArrowLeftIcon color={"white"} size={25} strokeWidth={1.7} />
+          </View>
+        </TouchableOpacity>
+      </SafeAreaView>
+      <View>
+        <TextInput
+          placeholder="Search..."
+          placeholderTextColor="#aaa"
+          className="border border-neutral-500 text-neutral-300 rounded-full py-2 px-4 text-base mt-2 mx-5"
+        />
+      </View>
+    </ScrollView>
   );
 };

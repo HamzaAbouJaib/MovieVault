01b8726ac5edf72d033fb7c0ffae99d6b8145d77
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-17 17:50:05 -0400

    Change drawer's default styling

--- navigation/AppNavigation.jsx
+++ navigation/AppNavigation.jsx
@@ -1,5 +1,7 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeStack from "./HomeStack";
+import { primaryTheme } from "../themes/primary";
+import { Text } from "react-native";
 
 const Drawer = createDrawerNavigator();
 
@@ -8,6 +10,12 @@ export function AppNavigation() {
     <Drawer.Navigator
       screenOptions={{
         headerShown: false,
+        drawerActiveBackgroundColor: "#27272a",
+        drawerActiveTintColor: primaryTheme.text,
+        drawerStyle: {
+          backgroundColor: "#18181b",
+          width: "60%",
+        },
       }}
     >
       <Drawer.Screen name="Home" component={HomeStack} />

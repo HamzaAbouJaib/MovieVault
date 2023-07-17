634e9cd2e5802da37e62129f1b6d2fa7171a94bc
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-17 17:34:49 -0400

    Remove navigation container

--- navigation/HomeStack.jsx
+++ navigation/HomeStack.jsx
@@ -1,4 +1,3 @@
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import MovieDetailScreen from "../screens/MovieDetailScreen";
@@ -10,34 +9,32 @@ const Stack = createNativeStackNavigator();
 
 export default function HomeStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          options={{ headerShown: false }}
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          name="MovieDetails"
-          options={{ headerShown: false }}
-          component={MovieDetailScreen}
-        />
-        <Stack.Screen
-          name="TVDetails"
-          options={{ headerShown: false }}
-          component={TVDetailScreen}
-        />
-        <Stack.Screen
-          name="PersonDetails"
-          options={{ headerShown: false }}
-          component={PersonDetailScreen}
-        />
-        <Stack.Screen
-          name="Search"
-          options={{ headerShown: false }}
-          component={SearchScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Main"
+        options={{ headerShown: false }}
+        component={HomeScreen}
+      />
+      <Stack.Screen
+        name="MovieDetails"
+        options={{ headerShown: false }}
+        component={MovieDetailScreen}
+      />
+      <Stack.Screen
+        name="TVDetails"
+        options={{ headerShown: false }}
+        component={TVDetailScreen}
+      />
+      <Stack.Screen
+        name="PersonDetails"
+        options={{ headerShown: false }}
+        component={PersonDetailScreen}
+      />
+      <Stack.Screen
+        name="Search"
+        options={{ headerShown: false }}
+        component={SearchScreen}
+      />
+    </Stack.Navigator>
   );
 }

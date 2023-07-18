4d4fb95da252e6a5e72aee84f0a22fef1aa5d8e2
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-17 20:50:13 -0400

    Display now playing, trending and top rated movies

--- screens/MoviesScreen.jsx
+++ screens/MoviesScreen.jsx
@@ -13,12 +13,45 @@ import {
   MagnifyingGlassIcon,
 } from "react-native-heroicons/outline";
 import { primaryStyles } from "../themes/primary";
+import {
+  fetchNowPlayingMovies,
+  fetchTopRatedMovies,
+  fetchTrendingMovies,
+  fetchUpcomingMovies,
+} from "../api/movies";
+import { useEffect, useState } from "react";
+import PosterList from "../components/PosterList";
 
 export default MoviesScreen = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+
   const navigation = useNavigation();
 
+  useEffect(() => {
+    getTrendingMovies();
+    getTopRatedMovies();
+    getNowPlayingMovies();
+  }, []);
+
+  async function getTopRatedMovies() {
+    const data = await fetchTopRatedMovies();
+    if (data && data.results) setTopRatedMovies(data.results);
+  }
+
+  async function getTrendingMovies() {
+    const data = await fetchTrendingMovies();
+    if (data && data.results) setTrendingMovies(data.results);
+  }
+
+  async function getNowPlayingMovies() {
+    const data = await fetchNowPlayingMovies();
+    if (data && data.results) setNowPlayingMovies(data.results);
+  }
+
   return (
-    <ScrollView className="flex-1 bg-zinc-950 pt-10">
+    <View className="flex-1 bg-zinc-950 pt-10">
       <SafeAreaView className="flex-row justify-between items-center mx-5 mb-3">
         <TouchableOpacity
           onPress={() => {
@@ -34,6 +67,16 @@ export default MoviesScreen = () => {
           <MagnifyingGlassIcon color={"white"} size={25} strokeWidth={1.7} />
         </TouchableOpacity>
       </SafeAreaView>
-    </ScrollView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: 10,
+        }}
+      >
+        <PosterList title="Now in Theatres" data={nowPlayingMovies} />
+        <PosterList title="Trending" data={trendingMovies} />
+        <PosterList title="Top Rated" data={topRatedMovies} />
+      </ScrollView>
+    </View>
   );
 };

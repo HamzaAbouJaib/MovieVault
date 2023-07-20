0e28aab60f671809f1d525ff1941180f1fb357c7
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-20 16:22:17 -0400

    Fetch and display favourited movies

--- screens/Favourites.jsx
+++ screens/Favourites.jsx
@@ -2,9 +2,29 @@ import { useNavigation } from "@react-navigation/native";
 import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
 import { Bars3BottomLeftIcon } from "react-native-heroicons/outline";
 import { primaryStyles } from "../themes/primary";
+import FavouritesContext from "../store/Favourites";
+import { useContext, useEffect, useState } from "react";
+import { fetchMovieDetails } from "../api/movies";
+import PosterList from "../components/PosterList";
 
 export default Favourites = () => {
+  const { favouriteMovies } = useContext(FavouritesContext);
+
+  const [movies, setMovies] = useState([]);
+
   const navigation = useNavigation();
+
+  useEffect(() => {
+    getMovies(favouriteMovies);
+  }, [favouriteMovies]);
+
+  async function getMovies(ids) {
+    setMovies([]);
+    for (const id of ids) {
+      const data = await fetchMovieDetails(id);
+      if (data) setMovies((prev) => [...prev, data]);
+    }
+  }
 
   return (
     <View className="flex-1 bg-zinc-950 pt-10">
@@ -23,6 +43,16 @@ export default Favourites = () => {
           <Bars3BottomLeftIcon color={"white"} size={25} strokeWidth={1.7} />
         </View>
       </SafeAreaView>
+      {movies && movies.length > 0 ? (
+        <PosterList title="Movies" data={movies} />
+      ) : (
+        <View className="ml-5">
+          <Text className="text-white font-semibold text-lg">Movies</Text>
+          <Text className="text-white text-base">
+            You have no favourited movies
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
